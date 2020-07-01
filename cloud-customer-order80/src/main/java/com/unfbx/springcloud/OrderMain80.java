cf26dcf150d0c0e190eaf7c670b51edcf1ba381c
guorutao <285012>
2020-07-02 00:57:59 +0800

    注册中心集群的搭建。服务注册到注册中心

--- cloud-customer-order80/src/main/java/com/unfbx/springcloud/OrderMain80.java
+++ cloud-customer-order80/src/main/java/com/unfbx/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.unfbx.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 描述：
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020-07-01
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- cloud-eureka-server7002/src/main/java/com/unfbx/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/unfbx/springcloud/EurekaMain7002.java
@@ -2,19 +2,19 @@ package com.unfbx.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 描述：
  *
  * @author grt
- * @date 2020-06-29
+ * @date 2020-07-02
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
